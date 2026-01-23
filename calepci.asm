e1e4ea63deb1a7237036f288d904bc770bf1381f
LIN-SHU-FAN <35457943+LIN-SHU-FAN@users.noreply.github.com>
2026-01-23 14:04:29 +0800

    Update calepci.asm
    FIX

--- calepci.asm
+++ calepci.asm
@@ -1,29 +1,35 @@
 include inc\Standard.inc
 include inc\File.inc
 include inc\Struct.inc
+
 .model small
 .586
+
 .data
-MSG1            db      10,13,'Please input Bus number->$'
-MSG2            db      10,13,'Please input Device number->$'
-MSG3            db      10,13,'Please input Function number->$'
-TransferTable   db      0ah, 0bh, 0ch, 0dh, 0eh, 0fh
-TransferTable1  db      'A', 'B', 'C', 'D','E', 'F'
-TransferTable2  db      'a', 'b', 'c', 'd','e', 'f'
-Data1           DB      31,0,31 DUP(?)
-BusData         db      ?
-DevData         db      ?
-FunData         db      ?
-PSP             dw      0
+MSG1            db 10,13,'Please input Bus number->$'
+MSG2            db 10,13,'Please input Device number->$'
+MSG3            db 10,13,'Please input Function number->$'
+
+TransferTable   db 0Ah,0Bh,0Ch,0Dh,0Eh,0Fh
+TransferTable1  db 'A','B','C','D','E','F'
+TransferTable2  db 'a','b','c','d','e','f'
+
+Data1           db 31,0,31 dup(?)
+BusData         db ?
+DevData         db ?
+FunData         db ?
+PSP             dw 0
 
 DeviceMsg   db 10,13,"Device ID = "
 DeviceData db 4 dup(0),'h','$'
 
 VendorMsg   db 10,13,"Vendor ID = "
 VendorData db 4 dup(0),'h','$'
+
 .stack
 .code
 
+;--------------------------------------------------
 Transfer Macro
         local   NotSmall, TransferOK, IsNumber
         cmp     al, 39h
@@ -39,177 +45,187 @@ IsNumber:
         and     al, 0fh
         jmp     TransferOK
 NotSmall:
-        mov     al, byte ptr TransferTable1
         sub     al, byte ptr TransferTable1
         mov     bl, al
         movzx   bx, bl
         mov     al, TransferTable[bx]
 TransferOK:
 ENDM
+
+;--------------------------------------------------
 Testa proc far
         push    ds
         xor     ax, ax
-        push    ax 
+        push    ax
+
         mov     ax, @data
         mov     ds, ax
-        mov     ax, es
-        mov     PSP, ax
-;        
+        mov     PSP, es
+
+;---------------- Bus ----------------
         Print   MSG1
         INPUT   Data1
         add     dx, 2
-int 1
         mov     bx, dx
         mov     ax, [bx]
-        cmp     ah, 0dh
-        ja      NotThing
-        mov     ah, 30h
+
+        cmp     ah, 0Dh
+        ja      Bus_OK
+        mov     ah, '0'
         xchg    ah, al
-NotThing:
+Bus_OK:
         Transfer
         xchg    ah, al
         Transfer
         shl     ah, 4
         or      ah, al
         mov     BusData, ah
-;
+
+;---------------- Device ----------------
         Print   MSG2
         INPUT   Data1
         add     dx, 2
         mov     bx, dx
         mov     ax, [bx]
-        cmp     ah, 0dh
-        ja      NotThing1
-        mov     ah, 30h
+
+        cmp     ah, 0Dh
+        ja      Dev_OK
+        mov     ah, '0'
         xchg    ah, al
-NotThing1:
+Dev_OK:
         Transfer
         xchg    ah, al
         Transfer
         shl     ah, 4
         or      ah, al
         mov     DevData, ah
-;        
+
+;---------------- Function ----------------
         Print   MSG3
         INPUT   Data1
         add     dx, 2
         mov     bx, dx
         mov     ax, [bx]
-        cmp     ah, 0dh
-        ja      NotThing2
-        mov     ah, 30h
+
+        cmp     ah, 0Dh
+        ja      Fun_OK
+        mov     ah, '0'
         xchg    ah, al
-NotThing2:
+Fun_OK:
         Transfer
         xchg    ah, al
         Transfer
         shl     ah, 4
         or      ah, al
         mov     FunData, ah
-;
-        mov     edx, 80h
-        Shl     edx, 8
-        Or      dl, BusData     ; Bus number 
-        Shl     edx, 5         
-        Or      dl, DevData     ; Device number
-        Shl     edx, 3          
-        Or      dl, FunData     ; Function number
-        Shl     edx, 8
-        mov     ebx, edx
-        
-        
-        and     ebx, 0FFFFFF00h     ; offset = 0x00
-        mov     eax, ebx
-        
+
+;--------------------------------------------------
+; *** FIXED PCI CONFIG ADDRESS BUILD ***
+;--------------------------------------------------
+        mov     eax, 80000000h          ; Enable bit (bit31)
+
+        movzx   ecx, BusData
+        shl     ecx, 16
+        or      eax, ecx               ; Bus
+
+        movzx   ecx, DevData
+        shl     ecx, 11
+        or      eax, ecx               ; Device
+
+        movzx   ecx, FunData
+        shl     ecx, 8
+        or      eax, ecx               ; Function
+
+        ; Offset = 0x00
+
         mov     dx, 0CF8h
-        out     dx, eax             ; write CONFIG_ADDRESS
+        out     dx, eax                ; CONFIG_ADDRESS
+
         mov     dx, 0CFCh
-        in      eax, dx             ; read CONFIG_DATA
+        in      eax, dx                ; CONFIG_DATA
 
-        
         call    DisplayError
-
-;       
         ret
 Testa endp
-;;---------------------------------------------------
+
+;--------------------------------------------------
 DisplayError proc
-    push    eax
-    push    ebx
-    push    ecx
-    push    edx
+        push    eax
+        push    ebx
+        push    ecx
+        push    edx
 
-    mov     ecx, eax            
+        mov     ecx, eax
 
-    ; =========================
-    ; Device ID = EAX[31:16]
-    ; =========================
-    mov     ebx, ecx
-    shr     ebx, 16             ; EBX = Device ID
+        ; Device ID [31:16]
+        mov     ebx, ecx
+        shr     ebx, 16
 
-    mov     dl, bh
-    call    HexToAscii
-    mov     DeviceData, dh
-    mov     DeviceData+1, dl
+        mov     dl, bh
+        call    HexToAscii
+        mov     DeviceData, dh
+        mov     DeviceData+1, dl
 
-    mov     dl, bl
-    call    HexToAscii
-    mov     DeviceData+2, dh
-    mov     DeviceData+3, dl
+        mov     dl, bl
+        call    HexToAscii
+        mov     DeviceData+2, dh
+        mov     DeviceData+3, dl
 
-    mov     ah, 9
-    mov     dx, offset DeviceMsg
-    int     21h
+        mov     ah, 9
+        mov     dx, offset DeviceMsg
+        int     21h
 
-    ; =========================
-    ; Vendor ID = EAX[15:0]
-    ; =========================
-    mov     ebx, ecx
-    and     ebx, 0FFFFh         ; EBX = Vendor ID
+        ; Vendor ID [15:0]
+        mov     ebx, ecx
+        and     ebx, 0FFFFh
 
-    mov     dl, bh
-    call    HexToAscii
-    mov     VendorData, dh
-    mov     VendorData+1, dl
+        mov     dl, bh
+        call    HexToAscii
+        mov     VendorData, dh
+        mov     VendorData+1, dl
 
-    mov     dl, bl
-    call    HexToAscii
-    mov     VendorData+2, dh
-    mov     VendorData+3, dl
+        mov     dl, bl
+        call    HexToAscii
+        mov     VendorData+2, dh
+        mov     VendorData+3, dl
 
-    mov     ah, 9
-    mov     dx, offset VendorMsg
-    int     21h
+        mov     ah, 9
+        mov     dx, offset VendorMsg
+        int     21h
 
-    pop     edx
-    pop     ecx
-    pop     ebx
-    pop     eax
-    ret
+        pop     edx
+        pop     ecx
+        pop     ebx
+        pop     eax
+        ret
 DisplayError endp
 
-;;---------------------------------------------------
-HexToAscii     proc
+;--------------------------------------------------
+HexToAscii proc
         push    cx
         push    dx
-        and     dl, 0fh
-        add     dl, 30h
-        cmp     dl, 3Ah
-        jb      No_Add_Seven
+
+        and     dl, 0Fh
+        add     dl, '0'
+        cmp     dl, '9'+1
+        jb      L1
         add     dl, 7
-No_Add_Seven:
+L1:
         mov     cl, dl
         pop     dx
-        and     dl, 0f0h
+
+        and     dl, 0F0h
         shr     dl, 4
-        add     dl, 30h
-        cmp     dl, 3Ah
-        jb      No_Add_Seven1
+        add     dl, '0'
+        cmp     dl, '9'+1
+        jb      L2
         add     dl, 7
-No_Add_Seven1:
+L2:
         mov     dh, dl
         mov     dl, cl
+
         pop     cx
         ret
-HexToAscii     endp
+HexToAscii endp
+
 end Testa
